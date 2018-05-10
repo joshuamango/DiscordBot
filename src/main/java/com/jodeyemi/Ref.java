d9d7268c32ea3d7acd44d6628aa53976e556d79f
Joshua <37916234+joshuamango@users.noreply.github.com>
2018-05-10 11:47:06 -0400

    Delete Ref.java
    Contained a private token

--- src/main/java/com/jodeyemi/Ref.java
+++
@@ -1,7 +0,0 @@
-package com.jodeyemi;
-
-public class Ref
-{
-    public static final String token = "NDQzNzk1NDM3NzQxMjc3MTg1.DdSkxw.yOVUBz1vKLoDBRfsyzigvm9JrQE";
-    public static final String prefix = ">";
-}
