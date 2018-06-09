98c40c081f894edbf1b50e28dda20dbc9e41f21c
Joshua <37916234+joshuamango@users.noreply.github.com>
2018-06-09 15:18:16 -0400

    Delete Ref.java

--- src/main/java/com/jodeyemi/Ref.java
+++
@@ -1,8 +0,0 @@
-package com.jodeyemi;
-
-class Ref
-{
-    static final String token = "NDQzNzk1NDM3NzQxMjc3MTg1.DdSkxw.yOVUBz1vKLoDBRfsyzigvm9JrQE";
-    static final String APIKey = "4c6bac44-9183-4505-b77d-4d7cd93756f9";
-    static final String prefix = ">";
-}
